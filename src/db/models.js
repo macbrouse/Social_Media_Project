6b4b25f0fac7a611c18df176dbefa4342fefa3da
Sarthak Varmani <macbrouse@gmail.com>
2023-05-09 19:15:19 +0530

    changes for hosting db

--- src/db/models.js
+++ src/db/models.js
@@ -1,10 +1,13 @@
+require ('dotenv').config()
 const Sequelize = require('sequelize')
 
 const db = new Sequelize({
     dialect: 'mysql',
-    database: 'cbsocialmediadb',
-    username: 'cbsocialuser',
-    password: 'cbsocialpass',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 })
 
 const COL_ID_DEF = {
